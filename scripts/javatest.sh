d5088f4feb5ade4aa8384521ba9f7a45be3023a2
Neo Hornberger <neo.hornberger@gmx.de>
2021-12-09 13:31:08 +0100

    debug printing (files)

--- scripts/javatest.sh
+++ scripts/javatest.sh
@@ -8,9 +8,16 @@ echo "LIBS = ${LIBS}"
 echo
 
 find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
+find . -name "*.java" #print sourcefiles
 time (find . -name "*.java" | xargs javac -cp "${LIBS}/*") #build classes
 echo "Java build time"
+echo
+
+find . -type f -name "*.class" #print classfiles
+echo
 
 time (java -jar "${LIBS}/junit-platform-console-standalone-1.8.1.jar" --classpath . --fail-if-no-tests --include-engine=junit-jupiter --include-classname='.*Test.*' --scan-classpath --reports-dir=reports 2>/dev/null | grep -wv "Thanks")
 echo "JUnit run-all time"
+echo
+
 find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
