f167cb794f62641783aa6b3364b6fcd27257fd21
Neo Hornberger <neo.hornberger@gmx.de>
2021-12-09 14:32:31 +0100

    allow multipe source folders

--- scripts/javatest.sh
+++ scripts/javatest.sh
@@ -1,23 +1,32 @@
-if [ -z ${JUNIT_DIR} ]; then
+if [ -z ${INPUT_JUNIT} ]; then
     LIBS="${INTERNAL_LIB}"
 else
-    LIBS="${JUNIT_DIR}"
+    LIBS="${INPUT_JUNIT}"
 fi
 
-echo "LIBS = ${LIBS}"
+echo "SOURCE = ${INPUT_SOURCE}"
+echo "LIBS   = ${LIBS}"
 echo
 
-find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
-find . -name "*.java" #print sourcefiles
-time (find . -name "*.java" | xargs javac -cp "${LIBS}/*") #build classes
-echo "Java build time"
-echo
+for SRC in ${INPUT_SOURCE}; do
+(
+    cd "${SRC}"
+    pwd
+    echo
 
-find . -type f -name "*.class" #print classfiles
-echo
+    find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
+    find . -name "*.java" #print sourcefiles
+    time (find . -name "*.java" | xargs javac -cp "${LIBS}/*") #build classes
+    echo "Java build time"
+    echo
 
-time (java -jar "${LIBS}/junit-platform-console-standalone-1.8.1.jar" --classpath . --fail-if-no-tests --include-engine=junit-jupiter --scan-classpath --reports-dir=reports 2>/dev/null | grep -wv "Thanks")
-echo "JUnit run-all time"
-echo
+    find . -type f -name "*.class" #print classfiles
+    echo
 
-find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
+    time (java -jar "${LIBS}/junit-platform-console-standalone-1.8.1.jar" --classpath . --fail-if-no-tests --include-engine=junit-jupiter --scan-classpath --reports-dir=reports 2>/dev/null | grep -wv "Thanks")
+    echo "JUnit run-all time"
+    echo
+
+    find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
+)
+done
