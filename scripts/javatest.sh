914221b2a7bfab2184147c7ebdef767a238e64ed
Neo Hornberger <neo.hornberger@gmx.de>
2021-12-09 14:49:48 +0100

    another fix

--- scripts/javatest.sh
+++ scripts/javatest.sh
@@ -1,32 +1,36 @@
 if [ -z ${INPUT_JUNIT} ]; then
-    LIBS="${INTERNAL_LIB}"
+    LIBS="${ACTION_PATH}/lib"
 else
-    LIBS="${INPUT_JUNIT}"
+    LIBS="$INPUT_JUNIT"
 fi
 
-echo "SOURCE = ${INPUT_SOURCE}"
-echo "LIBS   = ${LIBS}"
+TMP_DIR="$(mktemp -d)"
+
+echo "SOURCE  = $INPUT_SOURCE"
+echo "LIBS    = $LIBS"
+echo "TMP_DIR = $TMP_DIR"
 echo
 
 for SRC in ${INPUT_SOURCE}; do
 (
-    cd "${SRC}"
+    cd "$SRC"
+
+    for FILE in **/*.java; do
+        cp "$FILE" "$TMP_DIR"
+    done
+)
+done
+
+(
+    cd "$TMP_DIR"
     pwd
     echo
 
-    find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
-    find . -name "*.java" #print sourcefiles
     time (find . -name "*.java" | xargs javac -cp "${LIBS}/*") #build classes
     echo "Java build time"
-    echo
-
-    find . -type f -name "*.class" #print classfiles
     echo
 
     time (java -jar "${LIBS}/junit-platform-console-standalone-1.8.1.jar" --classpath . --fail-if-no-tests --include-engine=junit-jupiter --scan-classpath --reports-dir=reports 2>/dev/null | grep -wv "Thanks")
     echo "JUnit run-all time"
     echo
-
-    find . -type f -name "*.class" -exec rm {}  \;    #remove classfiles
 )
-done
